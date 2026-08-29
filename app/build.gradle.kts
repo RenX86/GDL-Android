5e8740db172af4c6a876117e320fb63e7e057cd3
RenX86 <134190723+RenX86@users.noreply.github.com>
2026-08-30 01:21:28 +0530

    Hot Patch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.renx86.gdlapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.2.1"
+        versionCode = 12
+        versionName = "0.2.2"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
@@ -33,8 +33,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
