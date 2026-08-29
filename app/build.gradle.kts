9d9e428ef9f708b11fa8cd26150d414539d09daf
RenX86 <134190723+RenX86@users.noreply.github.com>
2026-08-30 00:55:24 +0530

    Version Bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.renx86.gdlapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.2.0"
+        versionCode = 11
+        versionName = "0.2.1"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
